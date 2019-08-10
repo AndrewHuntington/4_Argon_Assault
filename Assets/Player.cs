b0a8b2bc05d05141dd7ad509dae957a82cae8c04
Andrew Huntington <andrew.huntington@gmail.com>
2019-08-10 22:21:58 +0900

    12 Input Sensitivity & Gravity
    Laid the foundation for horizontal movement of the player's ship (I think)

---
+++ Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In ms^-1 (meters per second)")] [SerializeField] float xSpeed = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffsetThisFrame = xThrow * xSpeed * Time.deltaTime;
+        print(xOffsetThisFrame);
+    }
+}
