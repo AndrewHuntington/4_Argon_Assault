c805f5eccf86a1d6b9bf62dadd4435f27a323c8a
Andrew Huntington <andrew.huntington@gmail.com>
2019-08-10 17:57:04 +0900

    8 Setup A Splash Screen
    Added music to splash scene
    Attached MusicPlayer script to Music Player obj to change levels

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke(nameof(LoadScene), 2f);
+    }
+
+    private void LoadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
