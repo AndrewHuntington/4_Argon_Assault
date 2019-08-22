2df2cc0e4489e09ecb83aab22add6cda329536f6
Andrew Huntington <andrew.huntington@gmail.com>
2019-08-23 00:21:15 +0900

    21 Project Tidying
    Cleaned up Assests folder
    Created SceneLoader.cs to handle scene transitions

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MusicPlayer : MonoBehaviour
+public class SceneLoader : MonoBehaviour
 {
-    private void Awake()
-    {
-        DontDestroyOnLoad(gameObject);
-    }
-
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +14,11 @@ public class MusicPlayer : MonoBehaviour
     private void LoadScene()
     {
         SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
