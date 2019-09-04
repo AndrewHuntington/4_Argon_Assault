f93d26bfb44f8071e3224da75dade4b91d70bb0b
Andrew Huntington <andrew.huntington@gmail.com>
2019-09-04 23:34:52 +0900

    24 Enable A Gameobject From C#
    Added Explosion GameObject to Player Ship
    Explosion activates when the player ship collides with an object

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -2,16 +2,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [Tooltip("In seconds")][SerializeField] float levelLoadDelay = 1f;
+    [Tooltip("FX prefab on player")][SerializeField] GameObject deathFX;
+
     private void OnTriggerEnter(Collider other)
     {
         StartDeathSequence();
+        deathFX.SetActive(true);
+        Invoke(nameof(ReloadScene), levelLoadDelay);
     }
 
     private void StartDeathSequence()
     {
-        gameObject.SendMessage("OnPlayerDeath");
+        SendMessage("OnPlayerDeath");
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(1);
     }
 }
